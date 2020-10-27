c5ceda8861d42c12567da3b447ea921051493bad
Thomas Cannon <nerdstoke@gmail.com>
2020-10-27 17:15:38 -0400

    works!

--- tweetiepie.js
+++ tweetiepie.js
@@ -1,29 +1,46 @@
 
-// https://github.com/tmijs/tmi.js#tmijs
-const tmi = require("tmi.js");
 const config = require("./config.js");
 const keys = require("./key.conf.js");
 
+// https://github.com/tmijs/tmi.js#tmijs
+const tmi = require("tmi.js");
+var Twitter = require('twitter');
+
 
 // setup client
-const client = new tmi.client({
+const twitch_client = new tmi.client({
   connection: {
-    secure: true,
+    // secure: true,
     reconnect: true,
+  },
+  identity: {
+    username: keys.twitch_bot_username,
+    password: keys.twitch_chat_oauth
   },
   channels: [config.channel],
 });
 
+const twitter_client = new Twitter({
+  consumer_key: keys.twitter_api_key,
+  consumer_secret: keys.twitter_secret_key,
+  access_token_key: keys.twitter_access_token,
+  access_token_secret: keys.twitter_access_token_secret
+});
+
+twitch_client.connect();
+
+
+
 var users = {}
 
 
-client.on("message", function aggCommands(channel, tags, message, self) {
+twitch_client.on("message", function aggCommands(channel, tags, message, self) {
   let user = tags.username;
   let can_tweet = true
   if (user.toLowerCase() != "streamelements") {
     let isCorrectChannel = `#${config.channel}` === channel;
     let messageMatches = message.match(/(^\!tweet\:\s)(.*)/) || message.match(/^(\!yes|\!no)$/) || message.match(/^(\!tweettime)$/);
-    if (self) return;
+    if (self) {return;}
     if (isCorrectChannel && messageMatches != null) {
       if (users[user] == undefined) {
         users[user] = {};
@@ -36,10 +53,10 @@ client.on("message", function aggCommands(channel, tags, message, self) {
       }
       if (messageMatches[1] == '!tweettime') {
         if (users[user]['last_tweet'] == undefined) {
-          send_to_twitch_chat(`@${user}, you can tweet now`);
+          twitch_client.say(keys.twitch_channel_name, `@${user}, you can tweet now`);
         } else {
           time_remaining = time_conversion(config.timeToWait - (new Date().getTime() - users[user]['last_tweet']));
-          send_to_twitch_chat(`@${user}, you can tweet again in ${time_remaining}`);
+          twitch_client.say(keys.twitch_channel_name, `@${user}, you can tweet again in ${time_remaining}`);
         }
       }
       if (can_tweet) { 
@@ -48,8 +65,10 @@ client.on("message", function aggCommands(channel, tags, message, self) {
           confirm_tweet(users[user]['tweet'],user);
         }
         else if (users[user]['tweet'] != null && messageMatches[1] == '!yes') {
-          send_to_twitter(users[user]['tweet'],user);
-          users[user]['last_tweet'] = new Date().getTime()
+          twitter_client.post('statuses/update', {status: users[user]['tweet']});
+          twitch_client.say(keys.twitch_channel_name, `@${user}, your tweet was sent to @Nerdstoke's twitter, you can tweet again in 24 hours`);
+          users[user]['last_tweet'] = new Date().getTime();
+          delete users[user]['tweet'];
         }
         else if (users[user]['tweet'] != null && messageMatches[1] == '!no') {
           cancel_tweet(users[user]['tweet'],user);
@@ -61,15 +80,15 @@ client.on("message", function aggCommands(channel, tags, message, self) {
 
 
 function confirm_tweet(tweet, user){
-  var confirmation = `@${user}, confirm tweet "${tweet}" to be sent to @NerdStoke twitter account. !yes to confirm, !no to cancel.`;
-  send_to_twitch_chat(confirmation);
+  var confirmation = `@${user}, confirm tweet: "${tweet}" to be sent to @NerdStoke twitter account. !yes to confirm, !no to cancel.`;
+  twitch_client.say(keys.twitch_channel_name, confirmation);
 };
 
 
 function cancel_tweet(tweet, user){
   delete users[user]['tweet'];
   var cancellation = `@${user}, you tweet was cancelled`;
-  send_to_twitch_chat(cancellation);
+  twitch_client.say(keys.twitch_channel_name, cancellation);
 };
 
 
@@ -88,27 +107,14 @@ function time_conversion(time) {
 };
 
 
-function send_to_twitch_chat(message) {
-  console.log(message);
-}
-
-
-function send_to_twitter(tweet, user){
-  var tweet_to_send = `@${user}, your tweet was sent to @Nerdstoke's twitter, you can tweet again in 24 hours`;
-  console.log(tweet_to_send);
-  delete users[user]['tweet'];
-};
-
-
-client.addListener("connected", function (address, port) {
+twitch_client.addListener("connected", function (address, port) {
   console.log("Connected! Waiting for messages..");
 });
 
-client.addListener("disconnected", function (reason) {
+twitch_client.addListener("disconnected", function (reason) {
   console.log("Disconnected from the server! Reason: " + reason);
 });
 
-client.connect();
 if (config.channel === 'nerdstoke') {
   console.log("\n'nerdstoke' is the default channel! Otherwise, run with the environment variable: ");
   console.log("TWITCH_CHANNEL=mychannelhere npm start\n");
